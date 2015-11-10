f260c84ad712b52cd2e6a6859efb1c090ecf71e5
Brooke <brooke.robinson@colorado.edu>
2015-11-09 18:59:11 -0700

    Updated.

--- Menu_Click.js
+++ Menu_Click.js
@@ -1,31 +1,3 @@
-//Found on TurnTheGameOn Youtube channel. 
-
-var isQuit=false;
-
-function OnMouseEnter(){
-//change text color
-renderer.material.color=Color.red;
-}
-
-function OnMouseExit(){
-//change text color
-renderer.material.color=Color.white;
-}
-
-function OnMouseUp(){
-//is this quit
-if (isQuit==true) {
-//quit the game
-Application.Quit();
-}
-else {
-//load level
-Application.LoadLevel(1);
-}
-}
-
-function Update(){
-//quit game if escape key is pressed
-if (Input.GetKey(KeyCode.Escape)) { Application.Quit();
-}
-}
+public void LoadStage()  {
+     Application.LoadLevel ("SceneTest");
+ }
